4bb9f971f418e72e0f2a1f90f6b47419ed666dea
Antonio Nazco <tonito.nazco@gmail.com>
2024-10-15 16:08:22 -0400

    Added Spring Boot Project

---
+++ menug/src/main/java/fiu/cen/menug/MenugApplication.java
@@ -0,0 +1,13 @@
+package fiu.cen.menug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenugApplication.class, args);
+	}
+
+}
---
+++ menug/src/test/java/fiu/cen/menug/MenugApplicationTests.java
@@ -0,0 +1,13 @@
+package fiu.cen.menug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
